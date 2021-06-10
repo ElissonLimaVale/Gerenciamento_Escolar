b2aba7235e985f8c284f6e9d5dbf216025c750f6
ElissonLimaVale <elissonllima1@gmail.com>
2021-06-10 00:47:24 -0300

    Adicionado viewcomponent usuario logout

---
+++ Data/Extensions/UserLogViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGIEscolar.Data.Service;
+using System.Threading.Tasks;
+
+namespace SGIEscolar.Data.Extensions
+{
+    public class UserLogViewComponent : ViewComponent
+    {
+        private readonly AutenticacaoService _user;
+        public UserLogViewComponent(AutenticacaoService user)
+        {
+            this._user = user;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewData["UserLog"] = _user.RetornaUsuarioEmail();
+            return View();
+        }
+    }
+}
