6cc5df78b087040fe4330f7d26377ac1514a0150
2Kinput <62145628+2Kinput@users.noreply.github.com>
2021-06-11 14:26:41 -0300

    feature Professores Iniciada

---
+++ wwwroot/js/professor/table.info.js
@@ -0,0 +1,77 @@
+﻿$(document).ready(() => {
+    $.ajax({
+        method: "Get",
+        url: "/Professor/Buscar",
+    }).done((data) => {
+        tableinfo.setData(data);
+    }).fail();
+});
+
+var columns = [
+    { title: "id", field: "id", visible: false },
+    { title: "Nome", field: "nome" },
+    { title: "Email", field: "email" },
+    { title: "Telefone", field: "telefone" },
+    { title: "Disciplina", field: "disciplina" },
+    { title: "Endereco", field: "endereco" },
+    { title: "Ações", field: "", formatter: ButtonTable, width: 140 }
+];
+
+var tableinfo = new Tabulator("#table-info", {
+    data: [],
+    height: "400px",
+    layout: "fitColumns",
+    columns: columns,
+    pagination: "local",
+    paginationSize: 10,
+    paginationSizeSelector: [20, 30, 40, 50],
+    placeholder: "Nenhum Professor Encontrado!",
+    dataLoaded: AddFunctionButtons,
+    locale: true,
+    langs: pt_br
+});
+
+function ButtonTable() {
+    return `
+        <div class="btn-group">
+            <button class="btn btn-xs btn-default editar" title="Editar Avaliação">
+                <i class="fa fa-pencil"></i>
+            </button>
+            <button class="btn btn-xs btn-default excluir" title="Excluir Avaliação">
+                <i class="fa fa-trash"></i>
+            </button>
+        </div>
+    `;
+}
+
+function AddFunctionButtons() {
+
+    $(".editar").click((e) => {
+        var row = $(e.currentTarget).parent().parent().parent();
+        var id = row.children()[0].innerText;
+        window.location.href = "/Professor/Editar/" + id;
+    });
+
+    $(".excluir").click((e) => {
+        var row = $(e.currentTarget).parent().parent().parent();
+        var id = row.children()[0].innerText;
+        bootbox.confirm({
+            title: "Te Certeza?",
+            message: "Deseja excluir esse Professor permanentemente? Esta ação é irrevercivel deseja continuar?"
+        }, () => {
+            window.location.href = "/Professor/Deletar/" + id;
+        });
+    });
+}
+
+$("#baixar").click(() => {
+    bootbox.dialogValue({
+        title: "Baixar PDF",
+        message: "Digite o nome do para o arquivo"
+    }, (data) => {
+        tableinfo.download("pdf", (data + ".pdf"), {
+            orientation: "portrait",
+            title: "Informações do Período do Professor"
+        });
+    });
+});
---
+++ wwwroot/js/professor/table.professor.js
@@ -0,0 +1,100 @@
+﻿$(document).ready(() => {
+    $.ajax({
+        method: "Get",
+        url: "/Professores/Buscar",
+    }).done((data) => {
+        tableprofessor.setData(data);
+    }).fail();
+});
+
+var methods = {
+    timeout: null,
+    search: (filtro) => {
+        $.ajax({
+            method: "Get",
+            url: "/Professores/Buscar",
+            data: {
+                filtro: filtro
+            }
+        }).done((data) => {
+            LoadHide();
+            tableprofessor.setData(data);
+        }).fail(() => {
+            LoadHide();
+            bootbox.error("Ops, ocorreu um erro inesperado ao tentar listar alunos, por favor atualize a página!");
+        });
+    }
+};
+
+var columns = [
+    { title: "id", field: "id", visible: false },
+    { title: "Nome", field: "nome" },
+    { title: "Email", field: "email" },
+    { title: "Telefone", field: "telefone" },
+    { title: "Disciplina", field: "disciplina" },
+    { title: "Endereco", field: "endereco" },
+    { title: "Ações", field: "", formatter: ButtonTable, width: 140 }
+];
+
+var tablealunos = new Tabulator("#table-professor", {
+    data: [],
+    height: "420px",
+    layout: "fitColumns",
+    columns: columns,
+    pagination: "local",
+    paginationSize: 12,
+    paginationSizeSelector: [20, 30, 40, 50],
+    placeholder: "Nenhum Professor Encontrado!",
+    dataLoaded: AddFunctionButtons,
+    locale: true,
+    langs: pt_br
+});
+
+function ButtonTable() {
+    return `
+        <div class="btn-group">
+            <button class="btn btn-xs btn-default editar" title="Editar Professor">
+                <i class="fa fa-pencil"></i>
+            </button>
+            <button class="btn btn-xs btn-default visualizar" title="Visualizar Professor">
+                <i class="fa fa-graduation-cap"></i>
+            </button>
+            <button class="btn btn-xs btn-default excluir" title="Excluir Professor">
+                <i class="fa fa-trash"></i>
+            </button>
+        </div>
+    `;
+}
+
+// Adiciona os códigos javascript para as ações dos botões depois que carrega o html da tabela
+function AddFunctionButtons() {
+
+    $(".editar").click((e) => {
+        var row = $(e.currentTarget).parent().parent().parent();
+        var id = row.children()[0].innerText;
+        window.location.href = "/Professor/Editar/" + id;
+    });
+
+    $(".visualizar").click((e) => {
+        var row = $(e.currentTarget).parent().parent().parent();
+        var id = row.children()[0].innerText;
+        window.location.href = "/Professor/InformacoesPeriodo/" + id;
+    });
+
+    $(".excluir").click((e) => {
+        var row = $(e.currentTarget).parent().parent().parent();
+        var id = row.children()[0].innerText;
+        bootbox.confirm({
+            title: "Tem Certeza?",
+            message: "Deseja excluir esse Professor permanentemente? Esta ação é irrevercivel deseja continuar?"
+        }, () => {
+            window.location.href = "/Professor/Deletar/" + id;
+        });
+    });
+}
+
+$("#search").keyup(() => {
+    LoadShow();
+    clearTimeout(methods.timeout);
+    methods.timeout = setTimeout(function () { methods.search($("#search").val()) }, 380);
+});
